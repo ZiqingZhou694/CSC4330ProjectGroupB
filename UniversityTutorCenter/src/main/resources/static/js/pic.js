b4e0f996805728749fa0a4313ae585e679e2af8e
ZiqingZhou694 <113397683+ZiqingZhou694@users.noreply.github.com>
2023-03-27 12:16:41 -0500

    spring boot setup
    setup that put the frontend and backend together(not test yet)

--- UniversityTutorCenter/src/main/resources/static/js/pic.js
+++
@@ -1,9 +0,0 @@
-
-window.onload=function() {
-
-    for (var i = 1; i <= 6; i++) {
-        var lbimg = document.getElementById('img');
-        lbimg.src = "../images/pics0" + i + ".jpg";
-
-    }
-}
